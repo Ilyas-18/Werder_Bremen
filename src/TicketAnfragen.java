86a794b7df58915e87c4a81bf8c7e88a96fdd290
Ilyas-18 <arouna.ilyas.z.2002@gmail.com>
2022-03-30 12:29:15 +0200

    to be continued

---
+++ src/TicketAnfragen.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class TicketAnfragen {
+    private JPanel TicketA;
+    private JPanel Eingaben;
+    private JPanel Rechnung;
+    private JRadioButton buRb;
+    private JRadioButton pRb;
+    private JComboBox katCb;
+    private JCheckBox schülerStudentRentnerCheckBox;
+    private JTextField textField1;
+}
